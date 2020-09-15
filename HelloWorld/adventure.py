e7ec97f8b2ec27af8a6b7cc6b3dbe62bbf3b8d16
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-09-15 14:36:08 -0700

    adventure with break

--- HelloWorld/adventure.py
+++ HelloWorld/adventure.py
@@ -3,4 +3,9 @@ available_exits = ["north", "south", "east", "west"]
 chosen_exit = ""
 while chosen_exit not in available_exits:
     chosen_exit = input("Please choose a direction: ")
+    if chosen_exit == "quit":
+        print("Game Over")
+        break
 print( "Aren't you glad you got out of there" )
+
+
