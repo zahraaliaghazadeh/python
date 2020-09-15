83221f07a5d756cbaf5a78c10f40ba44427b3fbd
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-09-15 14:39:17 -0700

    guessing game adventure with lower case

--- HelloWorld/adventure.py
+++ HelloWorld/adventure.py
@@ -2,8 +2,9 @@ available_exits = ["north", "south", "east", "west"]
 
 chosen_exit = ""
 while chosen_exit not in available_exits:
-    chosen_exit = input("Please choose a direction: ")
-    if chosen_exit == "quit":
+    chosen_exit = input("Please choose a direction, otherwise type quit: ")
+    # Quit will work as well
+    if chosen_exit.casefold() == "quit":
         print("Game Over")
         break
 print( "Aren't you glad you got out of there" )
