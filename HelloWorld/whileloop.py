db88a886e40ed8cb4b903ab4c9f8611abe0b2aa2
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-09-15 14:34:09 -0700

    adventure

---
+++ HelloWorld/whileloop.py
@@ -0,0 +1,15 @@
+# for loop
+# for i in range(10):
+#     print("i is not {}".format(i))
+
+# while loop
+i = 0
+while i < 10:
+    print("i is now {}".format(i))
+    i += 1
+
+
+# i = 0
+# while True:
+#     print("".format(i))
+#     i += 1
