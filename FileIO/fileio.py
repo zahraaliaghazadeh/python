ecce46413e26fc97671e49267db803813dfcf518
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-10-08 12:28:29 -0700

    reading text files

---
+++ FileIO/fileio.py
@@ -0,0 +1,40 @@
+jabber = open("sample.txt", 'r')
+# windows forward slash, mac and linux back slash
+# jabber = open("C:\\Documents and Settings\\tim\\My Documents\\sample.txt", 'r')
+# for line in jabber:
+#     if "jabberwock" in line.lower():
+#         print(line, end='')
+#
+# jabber.close()
+
+# with open("sample.txt", 'r') as jabber:
+#     for line in jabber:
+#         if "JAB" in line.upper():
+#             print(line, end='')
+#
+# with open("sample.txt", 'r') as jabber:
+#     line = jabber.readline()
+#     while line:
+#         print(line, end='')
+#         line = jabber.readline()
+
+# with open("sample.txt", 'r') as jabber:
+#     lines = jabber.readlines()
+# print(lines)
+#
+# for line in lines:
+#     print(line, end='')
+
+
+with open("sample.txt", 'r') as jabber:
+    lines = jabber.readlines()
+print(lines)
+
+for line in lines[::-1]:
+    print(line, end='')
+
+with open("sample.txt", 'r') as jabber:
+    lines = jabber.read()
+
+for line in lines[::-1]:
+    print(line, end='')
